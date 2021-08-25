dc2bd2666f0aee9460e708fd46dcd45bedf6f466
alphacpc <alphacpc@gmail.com>
2021-08-25 18:26:46 +0000

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import BottomNavigation from './navigation/BottomNavigation';
+import ResultScreen from './screens/ResultScreen';
+import { StatusBar } from 'react-native';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <StatusBar hidden />
+        <Stack.Navigator screenOptions={{ headerShown: false,}}>
+          <Stack.Screen name="BottomNavigation" component={BottomNavigation} />
+          <Stack.Screen name="ResultScreen" component={ResultScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
