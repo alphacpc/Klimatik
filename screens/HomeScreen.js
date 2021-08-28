63d46d583aca653a3de50d64c88a7ada245b1f7c
alphacpc <alphacpc@gmail.com>
2021-08-28 04:44:50 +0000

    get dynamic data from openweather

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,59 @@
-import React from 'react';
-import { View, Text, SafeAreaView, StyleSheet, Dimensions, ImageBackground, ScrollView} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, Text, SafeAreaView, StyleSheet, Dimensions, ImageBackground, ScrollView, FlatList} from 'react-native';
 import imgIcon from './../assets/Images/Icon.png';
 import Icon from 'react-native-vector-icons/Feather'
+import axios from 'axios';
 
 const HomeScreen = ({navigation}) => {
 
     const numTemps = [1,2,3,4,5];
+    const [datasCountry, setDatasCountry] = useState([]);
+    const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=14.732386&units=metric&lon=-17.432757&lang=fr&appid=60f53a068c9a251eb46dcca88b6b407c';
 
-    const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=14.732386&units=metric&lon=-17.432757&appid=60f53a068c9a251eb46dcca88b6b407c';
+    let days = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+    
+    const Hourly = ({item}) => {
+        return  <View style={HomeStyle.ThisDayTemp}>
+                    <Text style={HomeStyle.ThisDayTempTime}>{new Date(item.dt *1000).getHours()}H</Text>
+                    <Icon name="sun" color='orangered' size={30}/>
+                    <Text style={HomeStyle.ThisDayTempValue}>{Math.round(item.temp)}°</Text>
+                </View>
+    }
 
-    console.log(url)
+    const Daily = ({item}) => {
+        return  <View style={HomeStyle.NextDayItem}>
+                    <Text style={HomeStyle.NextDayItemText}>{days[new Date(item.dt * 1000).getDay()]}  </Text>
+                    <Icon name='sun' size={40} color='orangered'/>
+                    <View style={HomeStyle.NextDayItemTemps}>
+                        <Text style={HomeStyle.NextDayItemText,{color:'grey',marginLeft:10}}>{Math.round(item.temp.min)}°</Text>
+                        <Text style={HomeStyle.NextDayItemText}>{Math.round(item.temp.eve)}°</Text>
+                        <Text style={HomeStyle.NextDayItemText,{color:'grey',marginLeft:10}}>{Math.round(item.temp.max)}°</Text>
+                    </View>
+                </View>                    
+    }
+
+    const getDatas = async () => {
+        try{
+            const response = await axios.get(url);
+            const myData = await response.data;
+            console.log(myData.current.temp)
+            console.log(myData.current.weather[0].description)
+            setDatasCountry(myData);
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+    
+    useEffect( () => {
+        getDatas();
+    },[])
+
+    console.log(datasCountry)
+    console.log(datasCountry.current)
+    if(datasCountry.length == 0){
+        return <Text>Encours de chargement</Text>
+    }
 
     return (
         
@@ -17,8 +61,8 @@ const HomeScreen = ({navigation}) => {
             <SafeAreaView style={HomeStyle.PanelHead}>
                 <View style={HomeStyle.PanelHeadItem}>
                     <Text style={HomeStyle.CityName}>Dakar</Text>
-                    <Text style={HomeStyle.CurrentTemp}>18°</Text>
-                    <Text style={HomeStyle.CurrentWeather}>Nuagueux</Text>
+                    <Text style={HomeStyle.CurrentTemp}>{Math.round(datasCountry.current.temp)}</Text>
+                    <Text style={HomeStyle.CurrentWeather}>{datasCountry.current.weather[0].description}</Text>
                 </View>
 
                 <View style={HomeStyle.PanelHeadItem}>
@@ -28,43 +72,60 @@ const HomeScreen = ({navigation}) => {
 
             <SafeAreaView style={HomeStyle.MainContent}>
                 <View style={HomeStyle.TreeItem}>
-                    <Text style={HomeStyle.TreeItemText}><Icon name="cloud-rain" size={20} style={{marginRight: 4}}/> 13%</Text>
-                    <Text style={HomeStyle.TreeItemText}><Icon name="clock" size={20} style={{marginRight: 4}}/> 0.533mBar</Text>
-                    <Text style={HomeStyle.TreeItemText}><Icon name="wind" size={20} style={{marginRight: 4}}/> 9km/h</Text>
+                    <Text style={HomeStyle.TreeItemText}><Icon name="cloud-rain" size={20} style={{marginRight: 4}}/> {datasCountry.current.humidity}%</Text>
+                    <Text style={HomeStyle.TreeItemText}><Icon name="clock" size={20} style={{marginRight: 4}}/> {datasCountry.current.pressure / 1000}mBar</Text>
+                    <Text style={HomeStyle.TreeItemText}><Icon name="wind" size={20} style={{marginRight: 4}}/> {Math.round(datasCountry.current.wind_speed)} km/h</Text>
                 </View>
             
                 <View style={HomeStyle.TimesUpDown}>                    
-                    <Text style={HomeStyle.TimeText}>07:00 levé du soleil</Text>
-                    <Text style={HomeStyle.TimeText}> 18:00 couché du soleil</Text>
+                    <Text style={HomeStyle.TimeText}>{new Date(datasCountry.current.sunrise * 1000).toLocaleTimeString()} levé du soleil</Text>
+                    <Text style={HomeStyle.TimeText}> {new Date(datasCountry.current.sunset * 1000).toLocaleTimeString()} couché du soleil</Text>
                 </View>
 
 
                 <View style={HomeStyle.ThisDayBox}>
-                    <Text style={HomeStyle.ThisDayText}>Jeudi</Text>
-
+                    <Text style={HomeStyle.ThisDayText}>{days[new Date(datasCountry.current.dt*1000).getDay()]}</Text>
+                    
                     <View style={HomeStyle.ThisDayTemps}>
-                    {numTemps.map((value, index) => 
+
+                    <FlatList
+                        data={datasCountry.hourly}
+                        horizontal
+                        showsHorizontalScrollIndicator = {false}
+                        keyExtractor = {item => `${item.id}`}
+                        renderItem ={ ({item}) => <Hourly item={item} />}
+                    />
+
+                    {/* {numTemps.map((value, index) => 
                             <View key={index} style={HomeStyle.ThisDayTemp}>
                                 <Text style={HomeStyle.ThisDayTempTime}>12H</Text>
                                 <Icon name="sun" color='orangered' size={30}/>
                                 <Text style={HomeStyle.ThisDayTempValue}>19°</Text>
                             </View>
-                    ) }
+                    ) } */}
                     </View>
+
                 </View>
 
                 <View style={HomeStyle.NextDayItems}>
-                    
-                    {numTemps.map((value,index) => <View key={index} style={HomeStyle.NextDayItem}>
+                    <FlatList
+                        data={datasCountry.daily}
+                        showsVerticalScrollIndicator = {false}
+                        // showsHorizontalScrollIndicator = {false}
+                        keyExtractor = {item => `${item.id}`}
+                        renderItem ={ ({item}) => <Daily item={item} />}
+                    />
+                    {/* {numTemps.map((value,index) => <View style={HomeStyle.NextDayItem}>
                             <Text style={HomeStyle.NextDayItemText}>Vendredi</Text>
                             <Icon name='sun' size={40} color='orangered'/>
                             <View style={HomeStyle.NextDayItemTemps}>
+                                <Text style={HomeStyle.NextDayItemText,{color:'grey',marginLeft:10}}>14°</Text>
                                 <Text style={HomeStyle.NextDayItemText}>19°</Text>
                                 <Text style={HomeStyle.NextDayItemText,{color:'grey',marginLeft:10}}>14°</Text>
                             </View>
                         </View>                    
                     
-                    )}
+                    )} */}
 
 
                 </View>
